7a0475c722b525b28e20be3d08781b32426bce30
mingyizhang <zhangmy1990@gmail.com>
2020-06-17 16:20:06 -0500

    update whitenoise

--- InstaDemo/settings.py
+++ InstaDemo/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
+STATICFILES_DIRS = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 LOGIN_REDIRECT_URL= 'posts'
 
 LOGOUT_REDIRECT_URL = 'posts'
